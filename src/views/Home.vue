85346464b0bffe8123852741a63cde543d90bdb4
dprastha <danielprastha4@gmail.com>
2021-01-03 18:54:52 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <CarouselShayna />
+    <BannerShayna />
+    <InstagramShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue"
+import CarouselShayna from "@/components/CarouselShayna.vue"
+import BannerShayna from "@/components/BannerShayna.vue"
+import InstagramShayna from "@/components/InstagramShayna.vue"
+import PartnerShayna from "@/components/PartnerShayna.vue"
+import FooterShayna from "@/components/FooterShayna.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    CarouselShayna,
+    BannerShayna,
+    InstagramShayna,
+    PartnerShayna,
+    FooterShayna,
+  }
+}
+</script>
